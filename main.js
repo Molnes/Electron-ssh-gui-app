292d290026d8d85cbc5523d29b45243554eaabcd
jeckup907 <31732500+jeckup907@users.noreply.github.com>
2019-10-11 23:45:38 +0200

    Update main.js

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow){
+app.on('ready', createWindow); {
   autoUpdater.checkForUpdatesAndNotify();
 }
 
