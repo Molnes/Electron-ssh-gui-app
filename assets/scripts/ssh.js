cee394830082199be5fa25825a69f46cd36be6d2
Retardur <31732500+Retardur@users.noreply.github.com>
2019-10-16 15:31:33 +0200

    added uptime button

--- assets/scripts/ssh.js
+++ assets/scripts/ssh.js
@@ -59,3 +59,33 @@ function sshshutdown() {
         }
     }).start();
 }
+
+
+function sshuptime() {
+
+    var ssh = new SSH({
+        host: document.getElementById("ipadresse").value,
+        user: document.getElementById("brukernavn").value,
+        pass: document.getElementById("passord").value
+    });
+
+    ssh.exec('uptime', {
+        out: function (stdout) {
+            document.getElementById('consoleoutput').innerHTML = "";
+            document.getElementById('consoleoutput').innerHTML = stdout;
+            console.log(stdout);
+        },
+
+        err: function (stderr) {
+            document.getElementById('consoleoutputerror').innerHTML = "";
+            document.getElementById('consoleoutputerror').innerHTML = stderr;
+            console.log(stderr);
+        },
+
+        exit: function (code) {
+            document.getElementById('consoleoutputexitcode').innerHTML = "";
+            document.getElementById('consoleoutputexitcode').innerHTML = 'Exitcode ' + code;
+            console.log(code);
+        }
+    }).start();
+}
